3f447ac4d1c096042bceab85c3377a70cc59d56d
Maya <mayalic@post.bgu.ac.il>
2025-06-23 15:52:49 +0300

    added unit test for all methods in BoardBL and also flow test-3A PATTERN

--- BackendUnitTests/BoardBLTests.cs
+++ BackendUnitTests/BoardBLTests.cs
@@ -15,7 +15,8 @@ namespace BackendUnitTests
     {
         private BoardBL board;
         [SetUp]
-        public void Setup() {
+        public void Setup()
+        {
             AuthenticationFacade auth = new AuthenticationFacade();
             auth.Login("MayaL@post.bgu.ac.il");
             BoardFacade facade = new BoardFacade(auth);
@@ -24,34 +25,34 @@ namespace BackendUnitTests
         }
 
         [Test]
-        public void BoardNameIsCorrect()
+        public void BoardName_BoardNameIsCorrect()
         {
-            Assert.AreEqual("TestBoard",board.BoardName);
+            Assert.AreEqual("TestBoard", board.BoardName);
         }
 
         [Test]
-        public void OwnerEmailIsCorrect()
+        public void Owner_GetOwnerEmail_ReturnsCorrectEmail()
         {
             Assert.AreEqual("MayaL@post.bgu.ac.il", board.Owner);
         }
 
         [Test]
-        public void AddUserIsCorrect()
+        public void AddUser_UserIsAddedSuccessfully()
         {
             board.AddUser("newuser@post.bgu.ac.il");
             Assert.Contains("newuser@post.bgu.ac.il", board.Users);
         }
-      
+
         [Test]
-        public void AddUserTwice()
+        public void AddUser_AddDuplicateUser_ThrowsException()
         {
             board.AddUser("Duplicate@post.bgu.ac.il");
-            var ex=Assert.Throws<InvalidOperationException>(() => board.AddUser("Duplicate@post.bgu.ac.il"));
+            var ex = Assert.Throws<InvalidOperationException>(() => board.AddUser("Duplicate@post.bgu.ac.il"));
             Assert.That(ex.Message, Is.EqualTo("User Duplicate@post.bgu.ac.il is already a member of TestBoard"));
         }
 
         [Test]
-        public void RemoveExistUser()
+        public void RemoveUser_ExistingUser_IsRemovedSuccessfully()
         {
             board.AddUser("toRemove@post.bgu.ac.il");
             board.RemoveUser("toRemove@post.bgu.ac.il");
@@ -59,48 +60,48 @@ namespace BackendUnitTests
         }
 
         [Test]
-        public void RemoveNonExistUser()
+        public void RemoveUser_NonExistingUser_ThrowsException()
         {
             Assert.Throws<InvalidOperationException>(() => board.RemoveUser("nonexist@post.bgu.ac.il"));
         }
 
-        [Test] 
-        public void RemoveOwner()
+        [Test]
+        public void RemoveUser_OwnerCannotBeRemoved_ThrowsException()
         {
             Assert.Throws<InvalidOperationException>(() => board.RemoveUser("MayaL@post.bgu.ac.il"));
         }
 
         [Test]
-        public void TransferOwnership_Valid()
+        public void TransferOwnership_ValidUser_Succeeds()
         {
             board.AddUser("newOwner@post.bgu.ac.il");
             board.Owner = "newOwner@post.bgu.ac.il";
-            Assert.AreEqual("newOwner@post.bgu.ac.il",board.Owner);
+            Assert.AreEqual("newOwner@post.bgu.ac.il", board.Owner);
         }
 
-        [Test]  
-        public void TransferOwnership_ToNonExist_Invalid()
+        [Test]
+        public void TransferOwnership_NonMemberUser_ThrowsException()
         {
             var ex = Assert.Throws<Exception>(() => board.Owner = "not@post.bgu.ac.il");
             Assert.That(ex.Message, Does.Contain("not@post.bgu.ac.il is not a member"));
         }
 
         [Test]
-        public void TransferOwnership_ToNonTheOwner_Invalid()
+        public void TransferOwnership_SameAsCurrentOwner_ThrowsException()
         {
             var ex = Assert.Throws<Exception>(() => board.Owner = "MayaL@post.bgu.ac.il");
-            Assert.That(ex.Message,Is.EqualTo("MayaL@post.bgu.ac.il is already the owner of TestBoard"));
+            Assert.That(ex.Message, Is.EqualTo("MayaL@post.bgu.ac.il is already the owner of TestBoard"));
         }
 
         [Test]
-        public void AddTask_AddToBacklog()
+        public void AddTask_TaskAppearsInBacklog()
         {
             var task = board.addTask("task1", DateTime.Now.AddDays(3), "desc", 1);
             Assert.IsTrue(board.Columns[BoardBL.BACKLOG].GetTasks().ContainsKey(task.TaskID));
         }
 
         [Test]
-        public void MoveTask_BacklogToInProgress()
+        public void MoveTask_FromBacklogToInProgress_Succeeds()
         {
             var task = board.addTask("task1", DateTime.Now.AddDays(3), "desc", 1);
             board.moveTask(task.TaskID);
@@ -108,7 +109,7 @@ namespace BackendUnitTests
         }
 
         [Test]
-        public void MoveTask_InProgressToDone()
+        public void MoveTask_FromInProgressToDone_Succeeds()
         {
             var task = board.addTask("task1", DateTime.Now.AddDays(3), "desc", 1);
             board.moveTask(task.TaskID);
@@ -117,7 +118,7 @@ namespace BackendUnitTests
         }
 
         [Test]
-        public void MoveTaskFromDone_Invalid()
+        public void MoveTask_FromDoneColumn_ThrowsException()
         {
             var task = board.addTask("task1", DateTime.Now.AddDays(3), "desc", 1);
             board.moveTask(task.TaskID);
@@ -127,45 +128,45 @@ namespace BackendUnitTests
         }
 
         [Test]
-        public void MoveNonExistTask_Invalid()
+        public void MoveTask_NonExistentTask_ThrowsException()
         {
             var ex = Assert.Throws<Exception>(() => board.moveTask(999));
             Assert.That(ex.Message, Does.Contain("Task does not exist"));
         }
 
         [Test]
-        public void DeleteTask_Valid()
+        public void DeleteTask_ExistingTask_Succeeds()
         {
-            var task=board.addTask("task1",DateTime.Now.AddDays(3), "desc", 1);
-            var result=board.deleteTask(task.TaskID);
+            var task = board.addTask("task1", DateTime.Now.AddDays(3), "desc", 1);
+            var result = board.deleteTask(task.TaskID);
             Assert.IsTrue(result);
         }
 
         [Test]
-        public void DeleteNotExistTask_Invalid()
+        public void DeleteTask_NonExistentTask_ReturnsFalse()
         {
             var result = board.deleteTask(999);
             Assert.IsTrue(!result);
         }
 
         [Test]
-        public void ChangeMaxTask_Valid()
+        public void ChangeMaxTasks_ValidLimit_UpdatesSuccessfully()
         {
             board.changeMaxTasks(0, 10);
-            Assert.AreEqual(10,board.GetColumnLimit(0));
+            Assert.AreEqual(10, board.GetColumnLimit(0));
         }
 
         [Test]
-        public void ChangeMaxTask_Inalid()
+        public void ChangeMaxTasks_LimitBelowCurrentCount_ThrowsException()
         {
             board.addTask("task1", DateTime.Now.AddDays(3), "desc", 1);
             board.addTask("task2", DateTime.Now.AddDays(3), "desc", 2);
-            var ex = Assert.Throws<InvalidOperationException>(() => board.changeMaxTasks(0,1));
+            var ex = Assert.Throws<InvalidOperationException>(() => board.changeMaxTasks(0, 1));
             Assert.That(ex.Message, Does.Contain("Cant lower max tasks of backlog as it currently holds more than 1"));
         }
 
         [Test]
-        public void AssignTask_Valid()
+        public void AssignTask_ValidAssignee_Succeeds()
         {
             board.AddUser("assignee@post.bgu.ac.il");
             var task = board.addTask("task1", DateTime.Now.AddDays(3), "desc", 1);
@@ -174,23 +175,23 @@ namespace BackendUnitTests
         }
 
         [Test]
-        public void AssignTaskUserNotExist_Invalid()
+        public void AssignTask_AssigneeNotMember_ThrowsException()
         {
             var task = board.addTask("task1", DateTime.Now.AddDays(3), "desc", 1);
-            var ex= Assert.Throws<InvalidOperationException>(() => board.AssignTask("MayaL@post.bgu.ac.il", 0, task.TaskID, "nonAssignee@post.bgu.ac.il"));
+            var ex = Assert.Throws<InvalidOperationException>(() => board.AssignTask("MayaL@post.bgu.ac.il", 0, task.TaskID, "nonAssignee@post.bgu.ac.il"));
             Assert.That(ex.Message, Does.Contain("User nonAssignee@post.bgu.ac.il is a member of TestBoard"));
         }
 
         [Test]
-        public void AssignTaskTaskNotExist_Invalid()
+        public void AssignTask_TaskDoesNotExist_ThrowsException()
         {
             board.AddUser("assignee@post.bgu.ac.il");
-            var ex = Assert.Throws<InvalidOperationException>(() => board.AssignTask("MayaL@post.bgu.ac.il", 0,999, "nonAssignee@post.bgu.ac.il"));
+            var ex = Assert.Throws<InvalidOperationException>(() => board.AssignTask("MayaL@post.bgu.ac.il", 0, 999, "nonAssignee@post.bgu.ac.il"));
             Assert.That(ex.Message, Does.Contain("User nonAssignee@post.bgu.ac.il is a member of TestBoard"));
         }
 
         [Test]
-        public void AssignerIsNotAssignee_Invalid()
+        public void AssignTask_AssignerNotAssignee_ThrowsException()
         {
             board.AddUser("assignee1@post.bgu.ac.il");
             board.AddUser("assignee2@post.bgu.ac.il");
@@ -201,7 +202,7 @@ namespace BackendUnitTests
         }
 
         [Test]
-        public void FullTaskFlow_AssignmentAndMovement()
+        public void TaskFlow_AssignmentAndMovement_WorksCorrectly()
         {
             board.AddUser("assignee1@post.bgu.ac.il");
             var task = board.addTask("task1", DateTime.Now.AddDays(3), "desc", 1);
@@ -213,7 +214,7 @@ namespace BackendUnitTests
         }
 
         [Test]
-        public void FullTaskFlow_AssignmentAndReassign()
+        public void TaskFlow_AssignmentAndReassignment_WorksCorrectly()
         {
             board.AddUser("assignee1@post.bgu.ac.il");
             board.AddUser("assignee2@post.bgu.ac.il");
@@ -224,7 +225,7 @@ namespace BackendUnitTests
         }
 
         [Test]
-        public void FullTaskFlow_LimitFlow()
+        public void TaskFlow_MaxTasksLimit_ThrowsWhenFull()
         {
             board.changeMaxTasks(0, 2);
             board.addTask("task1", DateTime.Now.AddDays(2), "desc", 1);
@@ -234,7 +235,7 @@ namespace BackendUnitTests
         }
 
         [Test]
-        public void FullTransferOwnerFlow()
+        public void OwnershipFlow_TransferAndAddUser_WorksCorrectly()
         {
             board.AddUser("assignee1@post.bgu.ac.il");
             board.Owner = "assignee1@post.bgu.ac.il";
